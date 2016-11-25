e355f0f44ec07737bf66a5d800b6610b2a642ab2
khoatrancitynow <thanhkhoa.tran@citynow.vn>
2016-11-25 14:03:05 +0700

    Implement four

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @categories = Category.all
+    @categories_like = Category.all.where.not(id: current_user.category_users.like.pluck(:category_id).uniq)
+    @categories_unlike = Category.all.where.not(id: current_user.category_users.un_like.pluck(:category_id).uniq)
     @category_users = current_user.category_users.like
     @un_category_users = current_user.category_users.un_like
     p @category_users
